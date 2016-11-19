c0ffd332371a628b34fd9c4e5c36a5e9d863c94b
Marien Raat <marienraat@riseup.net>
2016-11-19 13:45:29 +0100

    Restart service on boot if necessary

---
+++ src/main/java/com/jmstudios/buzzer/state/BootReceiver.java
@@ -0,0 +1,46 @@
+// BootReceiver.java --- A class to receive boot events from the
+// Android operating system and restarts the service if needed.
+
+// Copyright (C) 2016 Marien Raat <marienraat@riseup.net>
+
+// Author: Marien Raat <marienraat@riseup.net>
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+package com.jmstudios.buzzer.state;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.jmstudios.buzzer.state.SharedPreferenceHelper;
+import com.jmstudios.buzzer.timing.BuzzAlarmScheduler;
+
+public class BootReceiver extends BroadcastReceiver {
+    private static final String TAG = "BootReceiver";
+    private static final boolean DEBUG = true;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (DEBUG) Log.d(TAG, "Boot event received");
+
+        SharedPreferenceHelper spHelper =
+            new SharedPreferenceHelper(context);
+
+        if (spHelper.isBuzzServiceOn())
+            BuzzAlarmScheduler.rescheduleAlarm(context);
+        else
+            BuzzAlarmScheduler.cancelAlarms(context);
+    }
+}
