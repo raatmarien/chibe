7ea20130ff81a9896e6272a0e9c5a3913904eab9
Marien Raat <marienraat@riseup.net>
2017-03-12 15:06:30 +0100

    Remove support for API 13 (Android 3.2)

--- src/main/java/com/jmstudios/chibe/timing/VibrationAlarmReceiver.java
+++ src/main/java/com/jmstudios/chibe/timing/VibrationAlarmReceiver.java
@@ -25,6 +25,7 @@ import android.content.Intent;
 import android.util.Log;
 import android.os.Vibrator;
 import android.app.NotificationManager;
+import android.annotation.TargetApi;
 
 import com.jmstudios.chibe.state.SettingsModel;
 
@@ -75,6 +76,7 @@ public class VibrationAlarmReceiver extends BroadcastReceiver {
         }
     }
 
+    @TargetApi(23)
     private boolean isDndEnabled(Context context) {
         NotificationManager notificationManager = (NotificationManager)
             context.getSystemService(Context.NOTIFICATION_SERVICE);
